649ce40c75853d3edaa1cb07f29847d355dcd97d
Mohamed Farahat Barakat Wahba <mohamed.97974@cs.mti.edu.eg>
2025-01-25 10:12:57 +0200

    Subject Class is Done

--- Program.cs
+++ Program.cs
@@ -106,6 +106,29 @@ namespace Exam
         }
     }
 
+    // Subject class
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public Exam SubjectExam { get; set; }
+
+        public Subject(int id, string name)
+        {
+            SubjectId = id;
+            SubjectName = name;
+        }
+
+        public void CreateExam(Exam exam)
+        {
+            SubjectExam = exam;
+        }
+
+        public override string ToString()
+        {
+            return $"Subject: {SubjectName} (ID: {SubjectId})";
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
