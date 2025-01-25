f2e43e119abb55571988e05e41bb92ae25d90d01
Mohamed Farahat Barakat Wahba <mohamed.97974@cs.mti.edu.eg>
2025-01-25 09:56:49 +0200

    Base Exam Class is Done

--- Program.cs
+++ Program.cs
@@ -61,6 +61,23 @@ namespace Exam
         }
     }
 
+    // Base Exam class
+    public abstract class Exam
+    {
+        public int Time { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public Exam(int time, int numberOfQuestions)
+        {
+            Time = time;
+            NumberOfQuestions = numberOfQuestions;
+            Questions = new List<Question>();
+        }
+
+        public abstract void ShowExam();
+    }
+
     internal class Program
     {
         static void Main(string[] args)
